e92411eba4db3407e93f6fa14519a24d3416e090
May Moftah <maymoftah@outlook.com>
2017-02-02 10:57:53 -0500

    Added 'write to csv' functionality for airodump-ng
    Signed-off-by: May Moftah <maymoftah@outlook.com>

--- wiley.py
+++ wiley.py
@@ -26,15 +26,17 @@ def init():
     wiFi.wmode = wInterface + 'mon'
     if wiFi.wmode and mode in iwconfig:
         print "Wireless Card already in Monitor Mode"
-        # print wiFi.wmode
+
     else:
         subprocess.call(["airmon-ng", "start", wiFi.wireless])
 
 
 def collect():
     print "All channels in range 1-11 on 2.5 GHz will be monitored by default"
-    subprocess.call(["airodump-ng", "-c", "1,2,3,4,5,6,7,8,9,10,11", wiFi.wireless])
+    subprocess.call(["airodump-ng", "-c", "1,2,3,4,5,6,7,8,9,10,11", "--output-format", "csv","--write", "csv",
+                     wiFi.wmode])
 
 
 if __name__ == '__main__':
     init()
+    collect()
