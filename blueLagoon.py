cbccda7530f51c3bc4b277347dfb738bd7e51e3d
May Moftah <maymoftah@outlook.com>
2017-02-14 16:33:31 -0500

    Added target() function - target() looks for bluelog files in current directory, matches to regex MAC address in the files and saves all MAC addresses in a set (no duplicates) then prints
    Signed-off-by: May Moftah <maymoftah@outlook.com>

--- blueLagoon.py
+++ blueLagoon.py
@@ -3,6 +3,7 @@ import subprocess
 import time
 import fnmatch
 import os
+import re
 
 
 def init():
@@ -17,14 +18,33 @@ def init():
 
 
 def scan():
-    seconds = 60
+    seconds = 30
     print 'Running Bluelog for ' + str(seconds) + ' seconds\n'
     bluelog = subprocess.Popen(["bluelog", "-c", "-m", "-n"])
     time.sleep(seconds)
     bluelog.terminate()
+    time.sleep(3)
+
+
+def target():
+    pattern = 'bluelog-*.log'
+    directory = os.listdir('.')
+    macAddress = re.compile("([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})")
+    logs = fnmatch.filter(directory, pattern)
+    macAddrArray = set()
+
+    for log in logs:
+        with open(log) as record:
+            for line in record:
+                matchResult = re.search(macAddress, line)
+                macAddrArray.add(matchResult.group(0))
+
+    for item in list(macAddrArray):
+        print item
 
 
 if __name__ == '__main__':
     init()
     time.sleep(3)
     scan()
+    target()
